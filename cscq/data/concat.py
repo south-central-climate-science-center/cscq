032c9b45755c162fb508b19970c29b0b02f2e744
Mark Stacy <mbstacy@gmail.com>
2015-05-07 09:15:06 -0500

    Allow for list of models to be inputed

--- cscq/data/concat.py
+++ cscq/data/concat.py
@@ -17,13 +17,33 @@ def ncrcat(parameter,domain,experiment,model,ensemble,base_output='/data/static_
     """    
     #Task ID and result directory 
     task_id = str(ncrcat.request.id)
-    resultDir = os.path.join(base_output, task_id)
+    resultDir = os.path.join(base_output, task_id,'/cmip5')
     os.makedirs(resultDir)
+    result={}
+    if isinstance(model,str):
+        model = [model]
+
+    for mdl in model:
+        #Make Model directory
+        out_dir = "%s/%s/%s" % (resultDir,parameter,mdl)
+        os.makedirs(out_dir)
+        #Get CMIP5 Metadata
+        files,times,outfile= get_cmip5_metadata(parameter,domain,experiment,mdl,ensemble)
+        #Concatenate CMIP5 file
+        docker_opts = "-v /data:/data"
+        docker_cmd = "ncrcat %s %s" % (" ".join(files), "%s/%s" % (out_dir,outfile))
+        try:
+            result = docker_task(docker_name="sccsc/netcdf",docker_opts=docker_opts,docker_command=docker_cmd,id=task_id)
+            #return "http://%s/sccsc_tasks/%s" % (result['host'],result['task_id'])
+        except:
+            raise
+    return "http://%s/sccsc_tasks/%s" % (result['host'],result['task_id'])
+
+def get_cmip5_metadata(parameter,domain,experiment,model,ensemble):
     # Web API Url
     url = "http://data.southcentralclimate.org/api/catalog/data/data_portal/cmip5_files/.json"
     url_params ="?page_size=0&query={'spec':{'variable':'%s','domain':'%s','experiment':'%s','ensemble':'%s','model':'%s'},'$orderby':{'time':1}}"
     url = "%s%s" % (url,url_params % (parameter,domain,experiment,ensemble,model))
-    
     response =requests.get(url)
     data = response.json()
     files=[]
@@ -34,25 +54,6 @@ def ncrcat(parameter,domain,experiment,model,ensemble,base_output='/data/static_
         times.extend(row['time'].split('-'))
         files.append("%s/%s.%s" % (row['path'],row['filename'],'nc'))
     times.sort()
-    filename="%s_%s-%s.nc" % ("".join(filename.split('_')[:-1]),times[0],times[-1])
+    filename="%s_%s-%s.nc" % ("_".join(filename.split('_')[:-1]),times[0],times[-1])
     files.sort()
-    #Concatenate file
-    docker_opts = "-v /data:/data"
-    docker_cmd = "ncrcat %s %s" % (" ".join(files), "%s/%s" % (resultDir,filename))
-    try:
-        result = docker_task(docker_name="sccsc/netcdf",docker_opts=docker_opts,docker_command=docker_cmd,id=task_id)
-        return "http://%s/sccsc_tasks/%s" % (result['host'],result['task_id'])
-    except:
-        raise
-
-    #cmd = ['ncrcat','-O', "%s/%s" % (resultDir,filename)]
-    #cmd.extend(files)
-    #print(cmd)
-    #call(cmd)
-    #return "http://data.southcentralclimate.org/sccsc_tasks/%s/%s" % (task_id,"%s/%s" % (resultDir,filename))
-
-
-
-    
-
-
+    return files,times,filename
