459f68bf7335472eb864aaa68bcfc1cc8d1b3318
Mark Stacy <mbstacy@gmail.com>
2016-12-31 14:38:38 -0600

    changed url to get metadata

--- cscq/data/concat.py
+++ cscq/data/concat.py
@@ -75,7 +75,7 @@ def ncrcat(parameter,domain,experiment,model,ensemble,base_output='/data/static_
 
 def get_cmip5_metadata(parameter,domain,experiment,model,ensemble):
     # Web API Url
-    url = "http://data.southcentralclimate.org/api/catalog/data/data_portal/cmip5_file/.json"
+    url = "http://data.southcentralclimate.org/api/catalog/data/catalog/cmip5_file/.json"
     url_params ="?page_size=0&query={'spec':{'variable':'%s','domain':'%s','experiment':'%s','ensemble':'%s','model':'%s'},'$orderby':{'time':1}}"
     url = "%s%s" % (url,url_params % (parameter,domain,experiment,ensemble,model))
     response =requests.get(url)
